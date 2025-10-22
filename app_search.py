ba0629513c6bcaa08e72c3ffd1a3c93bf1207e14
nunzo <l.nunnery11@gmail.com>
2025-10-22 10:18:34 -0400

    jsone decimal

--- app_search.py
+++ app_search.py
@@ -59,6 +59,8 @@ class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, date):
             return obj.isoformat()
+        if isinstance(obj, Decimal):
+            return float(obj)
         return super().default(obj)
 
 app.json_encoder = CustomJSONEncoder
