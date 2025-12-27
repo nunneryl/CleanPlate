e36d207018a23650a59185b064085aca55598d8f
nunzo <l.nunnery11@gmail.com>
2025-12-27 17:20:26 -0500

    Disable cache on recent-actions for debugging

--- app_search.py
+++ app_search.py
@@ -354,7 +354,7 @@ def get_restaurant_by_camis(camis):
     return jsonify(final_results[0])
 
 @app.route('/lists/recent-actions', methods=['GET'])
-@cache.cached(timeout=3600)
+# @cache.cached(timeout=3600)
 def get_recent_actions():
     # Temporary debug version - return empty arrays
     return jsonify({
